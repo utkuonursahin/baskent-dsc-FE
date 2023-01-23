d09a1c7c43180b79f6f0c0bae7c0a862ffdcfffc
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-23 19:10:46 +0300

    general improvement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,13 +7,12 @@ const Contact = () => {
       <h2 className="heading-2">İletişim</h2>
       <p>Sosyal medya hesaplarımızı takip edebilir, aşağıdaki buton ile direkt olarak bize ulaşabilirsin!</p>
       <div className="contact__socials">
-        <img src="/linkedin.svg" alt="linkedin icon"/>
-        <img src="/instagram.svg" alt="instagram icon"/>
-        <img src="/twitter.svg" alt="twitter icon"/>
-        <img src="/whatsapp.svg" alt="whatsapp icon"/>
-        <img src="/discord.svg" alt="discord icon"/>
+        <Link href="https://www.linkedin.com/company/developer-students-community/"><img src="/linkedin.svg" alt="linkedin icon"/></Link>
+        <Link href="https://www.instagram.com/baskentdsc/"><img src="/instagram.svg" alt="instagram icon"/></Link>
+        <Link href="https://chat.whatsapp.com/FDtofP4pVxgBmbiaCw6BXr"><img src="/whatsapp.svg" alt="whatsapp icon"/></Link>
+        <Link href="https://discord.gg/wBQnWdU"><img src="/discord.svg" alt="discord icon"/></Link>
       </div>
-      <button className="btn btn-cta"><Link href="https://docs.google.com/forms/d/e/1FAIpQLSf_oiFNow0vfISCxanxsqQeaQdzcLqihYpYT9CdfqoQyekXXg/viewform?usp=sf_link" target="_blank">Bize Ulaş</Link></button>
+      <button className="btn btn-cta"><Link href="https://docs.google.com/forms/d/e/1FAIpQLSf_oiFNow0vfISCxanxsqQeaQdzcLqihYpYT9CdfqoQyekXXg/viewform?usp=sf_link" target="_blank">Bİze Ulaş</Link></button>
     </section>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 const Hero = ({containerRef}) => {
   return (
     <section lang="tr" className="hero" ref={containerRef} id="hero">
-      <h1 className="heading-1">DERSLERLE YETİNMEYENLERE!</h1>
+      <h1 className="heading-1 heading-slogan">Develop Together</h1>
       <h2 className="heading-2">Hakkımızda</h2>
-      <p className="hero__description">
+      <p className="hero__description" lang="tr">
         DSC (Developer Students Community) ya da Türkçe ismiyle Yazılım Geliştirici Öğrenciler Topluluğu,
         yazılım sektöründe kariyer hedefleyen öğrencileri bir araya getirerek, ortak projeler geliştirmek,
         eğitimler ve seminerler düzenlemek, öğrencilerin mesleki çevresini genişletmek ve yarışmalara katılmak
-        amacıyla “Derslerle Yetinmeyenlere!” mottosuyla Ocak 2023&apos;de Başkent Üniversitesinde kurulmuş bir öğrenci
+        amacıyla “Develop Together!” mottosuyla Ocak 2023&apos;de Başkent Üniversitesinde kurulmuş bir öğrenci
         topluluğudur.
       </p>
     </section>
