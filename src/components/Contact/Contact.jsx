8297ef44a344fcdc270477953dc8f31ae53a3abb
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-31 00:19:23 +0300

    style update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from "next/link";
+import Image from "next/image";
 
 const Contact = () => {
   return (
     <section className="contact" id="contact">
+      <Image src="/hexagon.svg" alt="bg vector" width={100} height={100} className="background-vectors background-vectors--bottom"/>
       <h2 className="heading-2">İletişim</h2>
       <p>Sosyal medya hesaplarımızı takip edebilir, aşağıdaki buton ile direkt olarak bize ulaşabilirsin!</p>
       <div className="contact__socials">
